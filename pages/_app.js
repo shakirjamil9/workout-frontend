4acea340ce43d928bda44f4fff38d18c00dd3f33
shakirjamil9 <mianshakir9@hotmail.com>
2023-03-04 22:15:40 +0500

    dark theme removed

--- pages/_app.js
+++ pages/_app.js
@@ -3,21 +3,21 @@ import { QueryClientProvider, QueryClient } from 'react-query';
 import { Container, createTheme, Paper, ThemeProvider } from '@mui/material';
 
 const queryClient = new QueryClient();
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+// const darkTheme = createTheme({
+//   palette: {
+//     mode: 'dark',
+//   },
+// });
 function MyApp({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={darkTheme}>
-        <Paper>
-          <Container fixed maxWidth="md">
-            <Component {...pageProps} />
-          </Container>
-        </Paper>
-      </ThemeProvider>
+      {/* <ThemeProvider theme={darkTheme}> */}
+      <Paper>
+        <Container fixed maxWidth="md">
+          <Component {...pageProps} />
+        </Container>
+      </Paper>
+      {/* </ThemeProvider> */}
     </QueryClientProvider>
   );
 }
