57dea37900ca73002869ac9e6e1a0020ed55472a
shakirjamil9 <mianshakir9@hotmail.com>
2023-03-07 18:12:35 +0500

    env setup

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,7 @@
-export const baseURL = 'https://jealous-mittens-bee.cyclic.app';
+export const baseURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:4000'
+    : 'https://jealous-mittens-bee.cyclic.app';
 
 export const genders = [
   {
